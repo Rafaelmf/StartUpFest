e0ee8ff03b8ea718d8f98539d7be6aa15fe3d4cb
Rafael Freitas <rafaelmf1801@gmail.com>
2019-07-25 00:26:42 -0300

    Basic Structure for Fire Database

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import Firebase from './dataSource/fireBase';
 import { ALL_SEGMENTS } from './dataSource/requests';
-
 import Routes from './routes/Routes';
 
 const client = new ApolloClient({
@@ -10,11 +10,21 @@ const client = new ApolloClient({
 });
 
 function App() {
+  // Basic structure for graphql queries
   client
     .query({
       query: ALL_SEGMENTS,
     })
     .then(result => console.log(result));
+
+  // Basic structure for using firebase database
+  Firebase.database()
+    .ref('/users')
+    .once('value')
+    .then((snapshot) => {
+      console.log('values', snapshot.val());
+    });
+
   return (
     <ApolloProvider client={client}>
       <Routes />
