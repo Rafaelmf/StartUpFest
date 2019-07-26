24d128eb93c0a5c2a0225ef289e01a85ee35662d
Rafael Freitas <rafaelmf1801@gmail.com>
2019-07-25 23:30:04 -0300

    Added filter functions for checkbox and search bar. Image validation.

--- src/pages/startups/Startups.jsx
+++ src/pages/startups/Startups.jsx
@@ -1,8 +1,9 @@
 import React, { PureComponent } from 'react';
-import { Card, Input, Radio, Divider, Spin, Alert } from 'antd';
+import { Card, Input, Checkbox, Divider, Spin } from 'antd';
 import { withApollo } from 'react-apollo';
 import { ALL_SEGMENTS } from '../../dataSource/requests';
 import './Startups.css';
+import noLogo from './no-logo.jpg'; // Tell Webpack this JS file uses this image
 
 const { Meta } = Card;
 const { Search } = Input;
@@ -11,40 +12,81 @@ class Home extends PureComponent {
     super(props);
     this.state = {
       isLoading: true,
-      cardsData: [],
     };
   }
 
   componentDidMount() {
     const { client } = this.props;
-    console.log(this.props);
     client
       .query({
         query: ALL_SEGMENTS,
       })
       .then(result => {
+        //Todos os segmentos carregados na página
         const allSegments = result.data.allSegments;
-        this.setState({ isLoading: false, allSegments });
+        //Lista com os nomes dos segmentos para gerar os checkbox
+        const segmentNames = allSegments.map(segment => segment.name);
+        this.setState({
+          isLoading: false,
+          allSegments,
+          displaySegments: allSegments,
+          segmentNames,
+        });
       });
   }
 
-  onChangeRadio = e => {
-    console.log('radio checked', e.target.value);
-    this.setState({
-      radio: e.target.value,
-    });
+  onChangeCheckbox = checkedValues => {
+    const { allSegments } = this.state;
+    //Se nenhum checkbox está ativo, mostrar todos
+    if (checkedValues.length === 0) {
+      this.setState({
+        displaySegments: allSegments,
+      });
+    } else {
+      // Filtrar os segmentos cujo nome está na lista de checkbox ativos
+      const newDisplaySegments = allSegments.filter(segment =>
+        checkedValues.includes(segment.name),
+      );
+      this.setState({
+        displaySegments: newDisplaySegments,
+      });
+    }
+  };
+
+  onSearch = value => {
+    const { allSegments, isLoading } = this.state;
+    //Evitar uma busca antes de ter os dados carregados.
+    if (!isLoading) {
+      //Iterando em todos segmentos
+      const newDisplaySegments = allSegments.map(segments => {
+        //Filtrando as Startups que tem o nome contendo a string inserida no campo de busca
+        const filteredStartups = segments.Startups.filter(startup => {
+          return startup.name.toLowerCase().search(value.toLowerCase()) !== -1;
+        });
+        //Retornando o objeto segments com o campo Startups alterado
+        return {
+          ...segments,
+          Startups: filteredStartups,
+        };
+      });
+      this.setState({ displaySegments: newDisplaySegments });
+    }
   };
 
   render() {
-    const { isLoading, radio, allSegments } = this.state;
+    const { isLoading, displaySegments, segmentNames } = this.state;
     return (
       <div className="startup-container">
         <div className="search-header">
-          <Search placeholder="Pesquise o nome da Startup" onSearch={value => console.log(value)} />
-          <Radio.Group onChange={this.onChangeRadio} value={radio}>
-            {allSegments &&
-              allSegments.map(segment => <Radio value={segment.id}>{segment.name}</Radio>)}
-          </Radio.Group>
+          <Search
+            placeholder="Pesquise o nome da Startup"
+            onSearch={value => this.onSearch(value)}
+            enterButton
+          />
+          <div>
+            <span className="span-filter">Filtre por segmento:</span>
+            <Checkbox.Group options={segmentNames} onChange={this.onChangeCheckbox} />
+          </div>
         </div>
 
         <Divider />
@@ -54,14 +96,23 @@ class Home extends PureComponent {
             <Spin tip="Carregando..." />
           ) : (
             <div className="card-container">
-              {allSegments.map(segment =>
+              {displaySegments.map(segment =>
                 segment.Startups.map(startup => (
                   <Card
                     key={segment.id}
                     className="card"
                     hoverable
                     style={{ width: 300 }}
-                    cover={<img alt="example" src={startup.imageUrl} />}
+                    cover={
+                      <img
+                        onError={e => {
+                          e.target.onError = null;
+                          e.target.src = noLogo;
+                        }}
+                        alt="example"
+                        src={startup.imageUrl}
+                      />
+                    }
                   >
                     <Meta title={startup.name} description={segment.name} />
                   </Card>
